099f93b749589a6e74d031ff385df523667ba690
daowolf <daowolf@yandex.ru>
2024-04-02 17:45:17 +1000

    ex2

--- ex2.py
+++ ex2.py
@@ -11,14 +11,19 @@ def print_args(func):
 
 def sum_list(func):
     @functools.wraps(func)
-    def wrapper(a, b):
+    def wrapper(*args, **kwargs):
+        if len(args) !=2:
+            raise ValueError('Must be 2 lists')
+        if type(args[0]) == int:
+            return func(*args,**kwargs)
+        if len(args[0]) != len(args[1]):
+            raise ValueError('Lists has different length')
         c = list()
-        for x in range(0,len(a)):
-            c.append(a[x] + b[x]) 
+        for x in range(0,len(args[0])):
+            c.append(func(args[0][x], args[1][x])) 
 
         return c
     return wrapper
-
 
 @sum_list
 def sum(a,b):
@@ -26,3 +31,5 @@ def sum(a,b):
 
 res = sum([1,2,3],[1,2,3])
 print(res)
+
+print (sum(1,2))
